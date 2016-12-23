31c0ac2a233b608c347bca3276fb01ea4a1452c5
Lucas Cardoso <mr.lucascardoso@gmail.com>
2016-12-23 01:32:55 -0300

    rename package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='Awesome-mixins',
+    name='awesome-mixins',
     version='0.5',
     packages=find_packages(),
     install_requires=[
