fcacee25e93b6de4c8ec188766b402cee38421e3
Lucas Cardoso <mr.lucascardoso@gmail.com>
2016-12-23 01:34:57 -0300

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='awesome-mixins',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     install_requires=[
         'django-braces',
